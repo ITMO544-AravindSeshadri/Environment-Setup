ba879cf14dd48792d717198af4aa5e517cb1cc31
Aravind Seshadri <aravsesh1@gmail.com>
2015-10-01 20:50:17 -0500

    Made some changes to file

--- install-env.sh
+++ install-env.sh
@@ -2,6 +2,7 @@
 sudo apt-get update && sudo apt-get install -y apache2 git
 touch /tmp/hello.txt
 echo "Hello" > /tmp/hello.txt
-cp /home/controller/Documents/Application-Setup/Home.html /var/www/html
-cp /home/controller/Documents/Application-Setup/page2.html /var/www/html
-cp /home/controller/Documents/Application-Setup/images /var/www/html
+cd /var/www/html
+sudo git clone https://github.com/ITMO544-AravindSeshadri/Application-Setup.git
+sudo mv -v /var/www/html/Application-Setup/* /var/www/html/
+sudo rm -rf /var/www/html/Application-Setup
